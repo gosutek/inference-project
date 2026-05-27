92cc5193b5911c7c8479a444a81877a1156b659d
GosuTek <gosutek@gmail.com>
2026-05-27 13:54:17 +0300

    calc model byte size

--- src/main.c
+++ src/main.c
@@ -1,34 +1,46 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 #include "cJSON.h"
 
 #include "allocator.h"
 #include "helpers.h"
 
-void read_safetensor(char* safetensor_path)
+static i32 st_get_bsize(const char* safetensor_path, u64* bsize)
 {
-	FILE* file = fopen(safetensor_path, "r");
-	if (!file) {
-		fprintf(stderr, "couldn't load %s\n", safetensor_path);
+	struct stat st;
+	if (stat(safetensor_path, &st) == 0) {
+		*bsize = (u64)st.st_size;
+		return 1;
+	}
+	return 0;
+}
+
+int main(void)
+{
+	const char* model_filepath = "gemma-4-E4B-it/model.safetensors";
+	u64         st_bsize = 0;  // total file size of the safetensor in bytes
+	if (st_get_bsize(model_filepath, &st_bsize) != 1) {
+		fprintf(stderr, "couldn't read file size of %s\n", model_filepath);
 		exit(EXIT_FAILURE);
 	}
 
-	u64 header_len;
-	if (fread(&header_len, sizeof(uint64_t), 1, file) != 1) {
-		fprintf(stderr, "failed read\n");
+	FILE* file = fopen(model_filepath, "r");
+	if (!file) {
+		fprintf(stderr, "couldn't load %s\n", model_filepath);
+		exit(EXIT_FAILURE);
 	}
-	printf("Header size = %lu\n", header_len);
+
+	u64 header_bsize = 0;  // header size of the safetensor in bytes
+	if (fread(&header_bsize, sizeof header_bsize, 1, file) != 1) {
+		fprintf(stderr, "failed read\n");
+		exit(EXIT_FAILURE);
+	}
+
+	const u64 model_bsize = st_bsize - header_bsize - sizeof header_bsize;
 	fclose(file);
-}
 
-int main()
-{
-	read_safetensor("gemma-4-E4B-it/model.safetensors");
-	ExecCtx* ctx = NULL;
-
-	exec_ctx_create(&ctx);
-	exec_ctx_destroy(ctx);
 	return 0;
 }
