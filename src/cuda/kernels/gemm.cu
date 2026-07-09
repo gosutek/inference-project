1792f5c68c863fe3f5e4c419982ff97a3e183598
GosuTek <gosutek@gmail.com>
2026-07-09 14:04:06 +0300

    gemm v2

--- src/cuda/kernels/gemm.cu
+++ src/cuda/kernels/gemm.cu
@@ -1,17 +1,34 @@
 #include "gemm.cuh"
 
+/**
+ * This kernel gets its own separate file
+ * such that I can later improve on it
+ * and keep an archive of older versions
+**/
+
 __global__ void k_gemm(
-	const bf16* const __restrict__ a,
-	const bf16* const __restrict__ b,
-	bf16* const __restrict__ c,
+	const bf16* const __restrict__ a,  // expect rm
+	const bf16* const __restrict__ b,  // expect rm
+	bf16* const __restrict__ c,        // output rm
 	const u32 m,
 	const u32 k,
 	const u32 n)
 {
-	for (u32 i = threadIdx.x; i < k; i += blockDim.x) {
-		const bf16 val_a = _d_dn_rm_get(a, k, (u32)blockIdx.x, i);
-		const bf16 val_b = _d_dn_cm_get(b, k, i, (u32)blockIdx.x);
+	const u32 row = threadIdx.y + blockIdx.y * blockDim.y;
 
-		_d_dn_rm_set(c, n, (u32)blockIdx.x, i, val_a * val_b);
+	/**
+   * consecutive threadIdx.x's are assigned to the same warp (up to 32)
+   * since we treat that as the column we are essentially distributing
+   * a warp across the columns of C
+   * vvvvvvvvvvvvvvvvvvvvvvvvv
+   **/
+	const u32 col = threadIdx.x + blockIdx.x * blockDim.x;
+
+	for (u32 i = 0; i < k; ++i) {
+		const bf16 val_a = _d_dn_rm_get(a, k, row, i);
+		const bf16 val_b = _d_dn_rm_get(b, n, i, col);
+		const bf16 val_c = val_a * val_b;
+
+		_d_dn_rm_set(c, n, row, col, val_c);
 	}
 }
