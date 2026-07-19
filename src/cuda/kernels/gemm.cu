131d7d0888858810db1cdd5d66c42d1f87ce5c96
GosuTek <gosutek@gmail.com>
2026-07-19 18:18:39 +0300

    wip gemm v2 tranpose

--- src/cuda/kernels/gemm.cu
+++ src/cuda/kernels/gemm.cu
@@ -41,23 +41,29 @@ __global__ void k_gemmt(
 	const bf16* const __restrict__ a,  // expect rm
 	const bf16* const __restrict__ b,  // expect rm
 	bf16* const __restrict__ c,        // output rm
+	const u32 block_outer,
+	const u32 block_inner,
 	const u32 m,
 	const u32 k,
 	const u32 n)
 {
+	// NOTE: Check the n_cols of the tranposed $b$
+	extern __shared__ bf16 smem_a[];
+	bf16* const            smem_b = (bf16*)((u8*)smem_a + block_outer * block_inner * sizeof *smem_a);
+
+	const u32 col = threadIdx.x + blockIdx.x * blockDim.x;
 	const u32 row = threadIdx.y + blockIdx.y * blockDim.y;
 
-	/**
-   * consecutive threadIdx.x's are assigned to the same warp (up to 32)
-   * since we treat that as the column we are essentially distributing
-   * a warp across the columns of C
-   * vvvvvvvvvvvvvvvvvvvvvvvvv
-   **/
-	const u32 col = threadIdx.x + blockIdx.x * blockDim.x;
+	bf16 load_val = _d_dn_rm_get(a, k, row, col);
+	_d_dn_rm_set(smem_a, k, row, col, load_val);
+
+	load_val = _d_dn_rm_get(b, n, row, col);
+	_d_dn_rm_set(smem_b, k, col, row, load_val);  // transpose
+	__syncthreads();
 
 	bf16 acc = 0;
-	for (u32 i = 0; i < k; ++i) {
-		acc += _d_dn_rm_get(a, k, row, i) * _d_dn_rm_get(b, n, i, col);
+	for (u32 i = 0; i < block_inner; ++i) {
+		acc += _d_dn_rm_get(smem_a, k, row, i) * _d_dn_rm_get(smem_b, k, i, col);
 	}
 	_d_dn_rm_set(c, n, row, col, acc);
 }
